e463934338f172ae8d1263b0744cb9ad57b11ecc
srisai <srilekhab85@gmail.com>
2022-04-12 23:33:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@
 import streamlit
 streamlit.title('Hi, My name is Aishvariya. I Love Sri very much')
 streamlit.header('My mom\'s healthy diner breakfast recipes')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmestreamlit.dataframe(my_fruit_list)al')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
