8603d69fa3e4a6868bc8b463a177eef390aff8a2
srisai <srilekhab85@gmail.com>
2022-04-12 22:51:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 
 import streamlit
-streamlit.title('Hi, My name is Aishvariya. I Love Sri very much. Also there is a gigantic figure in this house. Its name is Dinesh -Devil')
+streamlit.title('Hi, My name is Aishvariya. I Love Sri very much')
