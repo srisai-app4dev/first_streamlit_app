a77ba6a3654f40fd53b517e5b66de7d2693860fe
srisai <srilekhab85@gmail.com>
2022-04-12 23:16:24 +0530

    Update streamlit_app.py
    Adding breakfast receipes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('Hi, My name is Aishvariya. I Love Sri very much')
+streamlit.header('My mom\'s healthy diner breakfast recipes')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
