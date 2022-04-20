fe0da8d25544da478d6880ff4512a59664e568e5
srisai <srilekhab85@gmail.com>
2022-04-20 18:42:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
