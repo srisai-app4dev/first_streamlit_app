e70d925476657f0e690e1ca65a2c35f08dc4d6e9
srisai <srilekhab85@gmail.com>
2022-04-21 15:57:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.write('The user entered',fruit_choice)
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.stop()
 add_my_fruit=streamlit.text_input('what fruit would you like to add?','apple')
 streamlit.write('The user entered the 2nd fruit as',add_my_fruit)
 
